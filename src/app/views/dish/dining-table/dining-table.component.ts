3a61d48e8d04df0e21088aab4b84f41f8afe6f99
rambhomkar <ram.bhomkar@gmail>
2022-02-21 11:37:39 +0530

    KOT changes

--- src/app/views/dish/dining-table/dining-table.component.ts
+++ src/app/views/dish/dining-table/dining-table.component.ts
@@ -1,8 +1,7 @@
-import {Component, EventEmitter,OnInit,Output} from '@angular/core';
-import {Hotel} from '../../../models/tabelConfiguration.model';
-import {CartService} from '../../../service/cart.service';
-import {TableService} from '../../../service/table.service';
-import { DishMenuNewComponent } from '../dish-menu-new/dish-menu-new.component';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Hotel } from '../../../models/tabelConfiguration.model';
+import { CartService } from '../../../service/cart.service';
+import { TableService } from '../../../service/table.service';
 
 @Component({
   selector: 'app-dining-table',
@@ -10,79 +9,65 @@ import { DishMenuNewComponent } from '../dish-menu-new/dish-menu-new.component';
   styleUrls: ['./dining-table.component.scss']
 })
 export class DiningTableComponent implements OnInit {
-  tableList: Hotel[];
-  displayModal: boolean;
+  tableList:Hotel[];
+  @Output() tableSelection = new EventEmitter<any>();
+  selectedTableID:Array<any> = [];
   displayBasic: boolean;
-  diningTableDialog:DishMenuNewComponent;
-  dinintab:any;
-  @Output() tableSelection = new EventEmitter < any > ();
-  selectedTableID: Array < any > = [];
-  tablesname: any[];
-  constructor(public tableSvc: TableService, private cartService: CartService) {}
-  ngOnInit() {
+  constructor(public tableSvc: TableService, private cartService: CartService) { }
+
+  ngOnInit(): void {
     this.selectedTableID = [];
-    this.dinintab=this.diningTableDialog;
     this.loadTabaleData();
-    this.tablesname = [{
-        label: '{{tblItem.name}}',
-        value: '!fullPrice'
-      },
-      {
-        label: 'Price Low to High',
-        value: 'fullPrice'
-      }
-    ];
   }
 
   loadTabaleData() {
     this.tableSvc.getTableData().subscribe(res => {
-      this.tableList = res;
+        this.tableList = res;
     })
-  }
-  fnTblBook(tblItem) {
-    this.selectedTableID = [];
-    if (tblItem.isBooked) {
+}
+  fnTblBook(tblItem){  
+    debugger
+      this.selectedTableID = [];
+    if(tblItem.isBooked){
       this.tableSvc.getOrderDataBytblId(tblItem.id).subscribe(resp => {
-        if (resp) {
+        if(resp){
           console.log(resp);
-          this.cartService.save(resp);
+          this.cartService.save(resp); 
         }
       })
     } else {
       tblItem.isBooked = true;
-      this.tableSvc.updateSeat(tblItem).subscribe(resp => {});
-      this.tableOperation(tblItem);
-    }
-     this.dinintab=false;
-    // this.ref.close(tblItem);
-  }
-  fnTblRelease(tblItem) {
-    tblItem.isBooked = false;
-
-    const index = this.selectedTableID.indexOf(tblItem.name, 0);
-    if (index > -1) {
-      this.selectedTableID.splice(index, 1);
-      this.cartService.tableOperation(this.selectedTableID);
-      this.tableSvc.updateSeat(tblItem).subscribe(resp => {});
-    }
-  }
-
-  tableOperation(tblItem) {
-    this.tableList.map((res: any) => {
-      if (res.name === tblItem.name) {
-        tblItem.isBooked = true;
-        if (this.selectedTableID.indexOf(tblItem.name) == -1) {
-          this.selectedTableID.push(tblItem.name)
-          this.cartService.addTable(tblItem);
-          this.cartService.tableOperation(this.selectedTableID);
-        }
-      }
+      this.tableSvc.updateSeat(tblItem).subscribe(resp =>{
     });
-
+  } 
+  this.tableList.map((res:any) => { 
+    if(res.name === tblItem.name ){
+      tblItem.isBooked = true; 
+      this.selectedTableID.push(tblItem.name)
+     this.cartService.addTable(tblItem); 
+    }
+  }); 
+  this.tableSelection.emit(this.selectedTableID)
+}
+fnTblRelease(tblItem) {
+  tblItem.isBooked = false;
+  debugger
+  // const index = this.selectedTableID.indexOf(tblItem.name, 0);
+  // if (index > -1) {
+  //   this.selectedTableID.splice(index, 1);
+    // this.cartService.tableOperation(this.selectedTableID);
+    this.tableSvc.updateSeat(tblItem).subscribe(resp => {});
+    this.tableList.map((res:any) => { 
+      if(res.name === tblItem.name ){
+        tblItem.isBooked = false; 
+      //   this.selectedTableID.push(tblItem.name)
+      //  this.cartService.addTable(tblItem); 
+      }
+    }); 
     this.tableSelection.emit(this.selectedTableID)
-  }
-
-  showBasicDialog() {
-    this.displayBasic = true;
-  }
+  // }
+} 
+showBasicDialog() {
+  this.displayBasic = true;
+}
 }
