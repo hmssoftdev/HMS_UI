438297afa0206885f661fb0a8a923f5cc002da38
fy5Software <88486409+fy5Software@users.noreply.github.com>
2022-06-02 12:06:52 +0530

    excel n pdf
    Signed-off-by: fy5Software <88486409+fy5Software@users.noreply.github.com>

--- src/app/views/dish/dish-category-config/dish-category-config.component.ts
+++ src/app/views/dish/dish-category-config/dish-category-config.component.ts
@@ -7,7 +7,9 @@ import { ShareDataService } from '../../../service/share-data.service';
 import { TranslateService } from "@ngx-translate/core";
 import { UserService } from '../../../service/user.service';
 import { CommonService } from '../../../service/common.service';
-
+import * as FileSaver from 'file-saver';
+import { DatePipe } from '@angular/common';
+// import { title } from 'process';
 @Component({
   selector: 'app-dish-category-config',
   templateUrl: './dish-category-config.component.html',
@@ -25,6 +27,12 @@ export class DishCategoryConfigComponent implements OnInit {
   dishList: Dish[];
   dish: Dish;
   lang:any;
+  exportColumns: any[];
+  cols:any[]
+  colss:any[]
+  pipe = new DatePipe('en-US');
+  todayWithPipe = null;
+
   constructor(
     private categorySvc: DishService,
     private msgService: MessageService,
@@ -42,6 +50,14 @@ export class DishCategoryConfigComponent implements OnInit {
    
 
   ngOnInit(): void {
+    this.todayWithPipe = this.pipe.transform(Date.now(), 'h:mm a');
+   
+    this.cols = [
+      { field: 'name', header: 'name' },
+      { field: 'gstCompliance', header: 'GST Compliance' },
+      { field: 'status', header: 'status' },
+   
+  ];
     this.authService.showLoader = true;
     this.status = [{ label: 'Active', value: 'active' },
     { label: 'InActive', value: 'inactive' }];
@@ -58,16 +74,43 @@ export class DishCategoryConfigComponent implements OnInit {
     //   console.log(x)
 
     // })
-    console.log("Hitting Language")
+    this.exportColumns = this.cols.map(col => ({title: col.header, dataKey: col.field}));
    
   }
   loadCategory() {
     this.shareData.currentId.subscribe(id => this.sendId = id);
     this.categorySvc.getDishCategory(this.sendId).subscribe(x => {
       this.CategoryList = x;
+      this.colss=this.CategoryList.map(res=> ({name:res.name, gst:res.gstCompliance,status:res.status}))
     this.authService.showLoader = false;
+    // this.exportColumns=this.CategoryList.map(res=>)
     });
   }
+  exportPdf() {
+    import("jspdf").then(jsPDF => {
+        import("jspdf-autotable").then(x => {
+            const doc = new jsPDF.default();
+            (doc as any).autoTable(this.exportColumns, this.CategoryList);
+            doc.save('Category_list.pdf');
+        })
+    })
+}
+exportExcel() {
+  import("xlsx").then(xlsx => {
+      const worksheet = xlsx.utils.json_to_sheet(this.colss);
+      const workbook = { Sheets: { 'data': worksheet }, SheetNames: ['data'] };
+      const excelBuffer: any = xlsx.write(workbook, { bookType: 'xlsx', type: 'array' });
+      this.saveAsExcelFile(excelBuffer, "Category");
+  });
+}
+saveAsExcelFile(buffer: any, Category: string): void {
+  let EXCEL_TYPE = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
+  let EXCEL_EXTENSION = '.xlsx';
+  const data: Blob = new Blob([buffer], {
+      type: EXCEL_TYPE
+  });
+  FileSaver.saveAs(data, Category + '_List_' + this.todayWithPipe + EXCEL_EXTENSION);
+}
 
   openNew() {
     this.category = new DishCategory();
--- src/app/views/report/backdata/backdata.component.ts
+++ src/app/views/report/backdata/backdata.component.ts
@@ -6,6 +6,7 @@ import { Historydata } from '../../../models/historydata';
 import { AuthService } from '../../../service/auth.service';
 import { EnumService } from '../../../service/enum.service';
 import { UserService } from '../../../service/user.service';
+import * as FileSaver from 'file-saver';
 @Component({
   selector: 'app-backdata',
   templateUrl: './backdata.component.html',
@@ -29,6 +30,10 @@ export class BackdataComponent implements OnInit {
   Dialog: boolean;
   num:number;
   invoice:number;
+  cols:any[];
+  exportColumns:any[];
+  colss:any[];
+  strVal:string;
   constructor(private msgService:MessageService,
     private auth :AuthService,private user:UserService,
      private enumService:EnumService,private confirmationService: ConfirmationService) { }
@@ -37,6 +42,15 @@ export class BackdataComponent implements OnInit {
     this.getHistorydata();
     this.ID=this.auth.userData().adminId;
     console.log(this.auth.userData(),"id")
+    this.cols = [
+      { field: 'userName', header: 'name' },
+      { field: 'userMobileNumber', header: 'contact' },
+      { field: 'itemCount', header: 'ItemCount' },
+     { field: 'grossTotal', header: 'Amount' },
+   
+   
+  ];
+  this.exportColumns = this.cols.map(col => ({title: col.header, dataKey: col.field}));
   }
   getHistorydata(){
     let maxnewDate = moment(this.maxDate).format('YYYY-MM-DD').toString();
@@ -49,25 +63,59 @@ export class BackdataComponent implements OnInit {
 
         console.log(this.historydata)
        this.loading  = false;
-
+      this.colss=this.historydata.map(res=> 
+        ({
+          userName:res.userName,
+          userMobileNumber:res.userMobileNumber,
+          itemCount:res.itemCount,
+          grossTotal:res.grossTotal,
+          mode:this.getPaymentMode(res.paymentMode)
+        }))
+    
       return this.historydata;
+
     }
   )
 
 
   }
+  exportPdf() {
+    import("jspdf").then(jsPDF => {
+        import("jspdf-autotable").then(x => {
+            const doc = new jsPDF.default();
+            (doc as any).autoTable(this.exportColumns, this.historydata);
+            doc.save('History-Data.pdf');
+        })
+    })
+}
+exportExcel() {
+  import("xlsx").then(xlsx => {
+      const worksheet = xlsx.utils.json_to_sheet(this.colss);
+      const workbook = { Sheets: { 'data': worksheet }, SheetNames: ['data'] };
+      const excelBuffer: any = xlsx.write(workbook, { bookType: 'xlsx', type: 'array' });
+      this.saveAsExcelFile(excelBuffer, "History-Data");
+  });
+}
+saveAsExcelFile(buffer: any, Category: string): void {
+  let EXCEL_TYPE = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
+  let EXCEL_EXTENSION = '.xlsx';
+  const data: Blob = new Blob([buffer], {
+      type: EXCEL_TYPE
+  });
+  FileSaver.saveAs(data, Category + '_List_' + EXCEL_EXTENSION);
+}
   getPaymentMode(n:number){
     this.num=n
-    let strVal= '-';
+  this.strVal= '-';
     switch(n){
       case 1:
-        strVal = 'Cash';
+        this.strVal = 'Cash';
         break;
       case 2:
-        strVal = 'UPI';
+        this.strVal = 'UPI';
         break;   
     }
-    return strVal;
+    return this.strVal;
   }
   fnhide(){
     this.Dialog=false
